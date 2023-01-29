a17bf7faf12ee2744e36e63cddb00188173a565a
Leandro Leme dos Santos <leandrolemesantos@gmail.com>
2023-01-28 23:22:16 -0300

    26º dia de CTD - Finalizando o projeto desafiador

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ function typeWriter(elemento) {
     const textoArray = elemento.innerHTML.split('');
     elemento.innerHTML = '';
     textoArray.forEach((letra, i) => {
-      setTimeout(() => elemento.innerHTML += letra, 320 * i);
+      setTimeout(() => elemento.innerHTML += letra, 225 * i);
     });
   }
 
