ec34a954753b9fc2cb82ea0de3a36e6bf158f256
Leandro Leme dos Santos <leandrolemesantos@gmail.com>
2023-01-29 22:28:22 -0300

    27º dia de CTD - FInalizando o Proejto Desafiador

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,30 @@ function typeWriter(elemento) {
     const textoArray = elemento.innerHTML.split('');
     elemento.innerHTML = '';
     textoArray.forEach((letra, i) => {
-      setTimeout(() => elemento.innerHTML += letra, 220 * i);
+      setTimeout(() => elemento.innerHTML += letra, 110 * i);
     });
   }
 
   const titulo = document.querySelector('h1');
   typeWriter(titulo);
+
+  //* animação 'p'*//
+
+  function typeWriter2(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+      setTimeout(() => elemento.innerHTML += letra, 70 * i);
+    });
+  }
+
+  const subtitulo = document.querySelector('p');
+  typeWriter2(subtitulo);
+
+  //*fade in imagem olhos*//
+
+  document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('body')
+            .classList
+            .add('loaded');
+  });
